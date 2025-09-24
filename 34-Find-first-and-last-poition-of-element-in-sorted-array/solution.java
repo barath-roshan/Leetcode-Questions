a52a9362d0e7a8e3ad92d6245342596ecbd4e372
Barath Roshan <barathkarur2006@gmail.com>
2025-09-24 11:37:11 +0530

    Create solution.java

---
+++ 34-Find-first-and-last-poition-of-element-in-sorted-array/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first=-1,last=-1;
+        int low=0,high=nums.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==target){
+                first=mid;
+                high=mid-1;;
+            }else if(target>nums[mid]){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+            low=0;high=nums.length-1;
+            while(low<=high){
+                int mid=(low+high)/2;
+                if(nums[mid]==target){
+                    last=mid;
+                    low=mid+1;
+                }else if(target>nums[mid]){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+            }
+            return new int[] {first,last};
+    }
+}
